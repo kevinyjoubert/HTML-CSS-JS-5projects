695403070cf8be06838045b53861aab6fe52bf95
Keviny Joubert <61211057+kevinyjoubert@users.noreply.github.com>
2020-08-18 19:42:21 -0300

    ex01 e ex02 done

---
+++ ex-js-proposto/ex02.js
@@ -0,0 +1,8 @@
+var n1 = prompt('Digite o primeiro valor:');
+var n2 = prompt('Digite o segundo valor:');
+n1 = parseInt(n1);
+n2 = parseInt(n2);
+
+alert(`O resto da divisao de ${n1} por ${n2} é ${n1 % n2}`);
+
+alert(`O numero ${n1} elevado a ${n2} é: ${n1 ** n2}`);
