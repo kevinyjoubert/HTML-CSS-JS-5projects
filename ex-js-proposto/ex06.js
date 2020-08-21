4b886e3971354fa0dd25aa5e171c8018fff65471
Keviny Joubert <61211057+kevinyjoubert@users.noreply.github.com>
2020-08-21 01:04:24 -0300

    ex06 done

---
+++ ex-js-proposto/ex06.js
@@ -0,0 +1,11 @@
+var ano = 1000;
+
+while (ano <= 2020) {
+    if (ano % 4 == 0 || ano % 400 == 0) {
+        document.write(`O ano ${ano} é bissexto<br>`);
+    };
+
+    ano += 1;
+};
+
+document.write('todos os anos bisextos até 2020');
