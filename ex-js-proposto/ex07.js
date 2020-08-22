85f0cc1628f49ea7ae3c0b01ee23200e7e8a8d10
Keviny Joubert <61211057+kevinyjoubert@users.noreply.github.com>
2020-08-22 19:21:14 -0300

    ex07

--- ex-js-proposto/ex07.js
+++ ex-js-proposto/ex07.js
@@ -1,1 +1,22 @@
 var output = document.querySelector('#output');
+var mensagem = ''
+
+function MostrarTabuada() {
+    var n = prompt('digite um numero: ');
+    n = parseInt(n);
+
+    var i = 1;
+    
+
+    while (i <= 1000) {
+        mensagem += `${n} x ${i} = ${n*i}<br>`;
+        i++;
+    };
+
+    output.innerHTML = mensagem;
+};
+
+function limparTabuada() {
+    mensagem = '';
+    output.innerHTML = mensagem;
+};
