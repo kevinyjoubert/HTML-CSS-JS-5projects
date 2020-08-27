46d1920f4d6b72634ac7142a3114e27fe5816b97
Keviny Joubert <61211057+kevinyjoubert@users.noreply.github.com>
2020-08-27 17:52:51 -0300

    ex09 done

---
+++ ex-js-proposto/ex09.js
@@ -0,0 +1,10 @@
+var largura = prompt('Digite a largura: ');
+var altura = prompt('Digite a altura: ');
+
+largura = parseFloat(largura);
+altura = parseFloat(altura);
+
+
+function mostrarArea(largura, altura) {
+    alert(`Área total: ${largura * altura}`);
+};
